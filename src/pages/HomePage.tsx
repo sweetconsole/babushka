56a5fd07419c5447b7d1c7ddc0005483cb148edd
sweetconsole <sweetconsole@gmail.com>
2025-03-18 22:33:37 +0300

    test

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,39 +1,58 @@
 import {FC} from "react";
-import Intro from "../components/Intro/Intro.tsx";
-import Info from "../components/Info/Info.tsx";
-import CasesPreview from "../components/Cases/CasesPreview.tsx";
-import Experience from "../components/Experiences/Experience.tsx";
-import Services from "../components/Services/Services.tsx";
+// import Intro from "../components/Intro/Intro.tsx";
+// import Info from "../components/Info/Info.tsx";
+// import CasesPreview from "../components/Cases/CasesPreview.tsx";
+// import Experience from "../components/Experiences/Experience.tsx";
+// import Services from "../components/Services/Services.tsx";
+//
+// import AdditionalServices from "../components/AdditionalServices/AdditionalServices.tsx";
+// import HowWork from "../components/HowWork/HowWork.tsx";
+// import BlogPreview from "../components/Blog/BlogsPreview.tsx";
+// import Feedback from "../components/Feedback/Feedback.tsx";
+// import Features from "../components/Features/Features.tsx";
 
-import AdditionalServices from "../components/AdditionalServices/AdditionalServices.tsx";
-import HowWork from "../components/HowWork/HowWork.tsx";
-import BlogPreview from "../components/Blog/BlogsPreview.tsx";
-import Feedback from "../components/Feedback/Feedback.tsx";
-import Features from "../components/Features/Features.tsx";
+// import FAQ from "../components/FAQ/FAQ.tsx";
+// import Footer from "../components/Footer/Footer.tsx";
 
-import FAQ from "../components/FAQ/FAQ.tsx";
-import Footer from "../components/Footer/Footer.tsx";
 
 const HomePage: FC = () => {
   return (
     <>
       <main>
-        <Intro />
-        <Info />
-        <CasesPreview />
-        <Experience />
-        <Services />
+        {/*<Intro />*/}
+        {/*<Info />*/}
+        {/*<CasesPreview />*/}
+        {/*<Experience />*/}
+        {/*<Services />*/}
 
-        <AdditionalServices />
-        <HowWork />
-        <BlogPreview />
-        <Feedback />
-        <Features />
+        {/*<AdditionalServices />*/}
+        {/*<HowWork />*/}
+        {/*<BlogPreview />*/}
+        {/*<Feedback />*/}
+        {/*<Features />*/}
 
-        <FAQ />
+        {/*<FAQ />*/}
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer lobortis ante sed tellus accumsan pharetra eget eget augue. Donec nec lorem eget enim tristique malesuada eu in magna. Vestibulum nec congue ipsum. Etiam bibendum, felis eu sodales blandit, lacus ipsum consequat mi, non elementum nisl mi in eros. Integer magna mauris, malesuada ut neque vitae, tempus consectetur ante. Mauris eget tellus non nunc eleifend tincidunt quis non nisi. Suspendisse quis odio facilisis, vehicula libero non, accumsan magna. Nulla eu nibh nec felis semper dapibus. Aenean fermentum tristique eros nec egestas. Fusce quis urna at sem convallis scelerisque. In non maximus magna, ornare rhoncus mauris.
+
+          Vestibulum at tellus a nisi tristique porta. Duis vel mattis eros, non imperdiet lorem. Suspendisse commodo fringilla sem ut laoreet. Donec nulla neque, eleifend et quam ut, posuere efficitur nisi. Vestibulum ultrices cursus tincidunt. Donec lobortis hendrerit velit, et tristique metus finibus rutrum. Quisque eget nisl sit amet diam efficitur volutpat sed nec sapien. Fusce varius feugiat mi, eget sollicitudin dolor lobortis vestibulum. Mauris molestie arcu a bibendum rhoncus.
+
+          Donec eros tellus, scelerisque eu tellus eget, pulvinar semper felis. Curabitur dignissim lorem turpis, ac interdum purus pellentesque vitae. Nulla eget mauris et lorem dictum lobortis. In vitae aliquam ex, id venenatis arcu. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sit amet luctus orci, ut sollicitudin justo. Phasellus molestie tellus nisl, ut tempus diam ornare nec. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed eget posuere eros, at tincidunt nibh. Integer porttitor ipsum a eros ornare, tincidunt commodo metus mattis. Nullam nibh est, mattis eget posuere sit amet, facilisis sed felis. Maecenas sed pulvinar lacus, in pellentesque diam. Vivamus venenatis fringilla velit eget pharetra.
+
+          Nullam eget metus ac nulla egestas imperdiet vitae ut libero. Morbi molestie, orci vitae ullamcorper egestas, tellus sapien elementum lacus, nec rutrum orci urna non eros. Aliquam quis eleifend dui, non vestibulum mauris. Praesent in justo diam. Mauris sit amet ultrices turpis. Maecenas et mauris facilisis, gravida quam ac, malesuada massa. Duis vel odio dapibus, ornare sem in, efficitur nibh. Etiam tincidunt ultricies aliquet.
+
+          Cras pulvinar semper magna sit amet imperdiet. Donec posuere laoreet convallis. Vestibulum aliquet tellus hendrerit, congue sapien eget, tempor quam. Morbi efficitur scelerisque ex, ac posuere elit aliquam vitae. Ut placerat congue enim eu tincidunt. Nullam quam nunc, mattis a dolor ut, blandit feugiat leo. Nulla scelerisque euismod consectetur. Fusce rhoncus rhoncus iaculis. Nulla dignissim lectus sed ipsum malesuada, id dictum justo finibus. Donec vel mi nec neque ultricies congue. Duis aliquam malesuada mauris, a rhoncus risus maximus a. Praesent viverra, risus et porttitor hendrerit, tellus lectus volutpat nisi, vitae pellentesque augue libero quis sapien.<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer lobortis ante sed tellus accumsan pharetra eget eget augue. Donec nec lorem eget enim tristique malesuada eu in magna. Vestibulum nec congue ipsum. Etiam bibendum, felis eu sodales blandit, lacus ipsum consequat mi, non elementum nisl mi in eros. Integer magna mauris, malesuada ut neque vitae, tempus consectetur ante. Mauris eget tellus non nunc eleifend tincidunt quis non nisi. Suspendisse quis odio facilisis, vehicula libero non, accumsan magna. Nulla eu nibh nec felis semper dapibus. Aenean fermentum tristique eros nec egestas. Fusce quis urna at sem convallis scelerisque. In non maximus magna, ornare rhoncus mauris.
+
+            Vestibulum at tellus a nisi tristique porta. Duis vel mattis eros, non imperdiet lorem. Suspendisse commodo fringilla sem ut laoreet. Donec nulla neque, eleifend et quam ut, posuere efficitur nisi. Vestibulum ultrices cursus tincidunt. Donec lobortis hendrerit velit, et tristique metus finibus rutrum. Quisque eget nisl sit amet diam efficitur volutpat sed nec sapien. Fusce varius feugiat mi, eget sollicitudin dolor lobortis vestibulum. Mauris molestie arcu a bibendum rhoncus.
+
+            Donec eros tellus, scelerisque eu tellus eget, pulvinar semper felis. Curabitur dignissim lorem turpis, ac interdum purus pellentesque vitae. Nulla eget mauris et lorem dictum lobortis. In vitae aliquam ex, id venenatis arcu. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sit amet luctus orci, ut sollicitudin justo. Phasellus molestie tellus nisl, ut tempus diam ornare nec. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed eget posuere eros, at tincidunt nibh. Integer porttitor ipsum a eros ornare, tincidunt commodo metus mattis. Nullam nibh est, mattis eget posuere sit amet, facilisis sed felis. Maecenas sed pulvinar lacus, in pellentesque diam. Vivamus venenatis fringilla velit eget pharetra.
+
+            Nullam eget metus ac nulla egestas imperdiet vitae ut libero. Morbi molestie, orci vitae ullamcorper egestas, tellus sapien elementum lacus, nec rutrum orci urna non eros. Aliquam quis eleifend dui, non vestibulum mauris. Praesent in justo diam. Mauris sit amet ultrices turpis. Maecenas et mauris facilisis, gravida quam ac, malesuada massa. Duis vel odio dapibus, ornare sem in, efficitur nibh. Etiam tincidunt ultricies aliquet.
+
+            Cras pulvinar semper magna sit amet imperdiet. Donec posuere laoreet convallis. Vestibulum aliquet tellus hendrerit, congue sapien eget, tempor quam. Morbi efficitur scelerisque ex, ac posuere elit aliquam vitae. Ut placerat congue enim eu tincidunt. Nullam quam nunc, mattis a dolor ut, blandit feugiat leo. Nulla scelerisque euismod consectetur. Fusce rhoncus rhoncus iaculis. Nulla dignissim lectus sed ipsum malesuada, id dictum justo finibus. Donec vel mi nec neque ultricies congue. Duis aliquam malesuada mauris, a rhoncus risus maximus a. Praesent viverra, risus et porttitor hendrerit, tellus lectus volutpat nisi, vitae pellentesque augue libero quis sapien.</p></p>
       </main>
 
-      <Footer />
+      {/*<Footer />*/}
     </>
   )
 }
