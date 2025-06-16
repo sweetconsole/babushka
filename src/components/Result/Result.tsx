cde8c012c8184258e636203eafc59b13cd09b90d
sweetconsole <sweetconsole@gmail.com>
2025-06-16 22:20:10 +0300

    Добавление ссылки на сайт в блок <<Результат>>

--- src/components/Result/Result.tsx
+++ src/components/Result/Result.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react"
 import Tangle from "../../assets/images/tangles/tangle_5.svg"
 import TangleMobile from "../../assets/images/tangles/tangle_6.svg"
+import Arrow from "../../assets/images/arrow.svg"
 import { IResultProps } from "./result.interface.ts"
 import styles from "./Result.module.scss"
 
@@ -17,6 +18,19 @@ const Result: FC<IResultProps> = ({title, subtitle, description, link}) => {
 				<h3 className={styles.title}>{title}</h3>
 				<p className={styles.subtitle}>{subtitle}</p>
 				<p className={styles.description}>{description}</p>
+
+				<>
+					{link != undefined ? (
+						<a className={styles.link} href={link} target="_blank" rel="noopener noreferrer">
+							<div className={styles.link_text}>открыть сайт</div>
+							<div className={styles.link_arrow}>
+								<img className={styles.link_arrow_icon} src={Arrow} alt="^" />
+							</div>
+						</a>
+					) : (
+						<></>)
+					}
+				</>
 			</div>
 		</div>
 	)
